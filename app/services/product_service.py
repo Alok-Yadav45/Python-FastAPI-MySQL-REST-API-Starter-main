b4a72c8904bad07ccadb880af2e42052f2cd4e9d
Alok-Yadav45 <alokyadav@tothebyte.com>
2025-09-05 17:01:10 +0530

    fixed product helper and service

--- app/services/product_service.py
+++ app/services/product_service.py
@@ -1,46 +1,52 @@
 from sqlalchemy.orm import Session
-from fastapi import HTTPException
-from app.models.product_category import Category
-from app.models.product_model import Product
-from app.schemas.product_schema import ProductCreate, ProductUpdate
+from fastapi import status
+from app.helpers import product_helper
+from app.schemas.product_schema import ProductCreate, ProductUpdate, ProductResponse
+
+from app.helpers.response_helper import success_response
+from app.helpers.exceptions import CustomException
 
 def create_product(db: Session, product_data: ProductCreate):
-    category = db.query(Category).filter(Category.id == product_data.category_id).first()
-    if not category:
-        raise HTTPException(status_code=400, detail="Invalid category_id: category does not exist")
+    product = product_helper.create_product(db, product_data)
+    if not product:
+        raise CustomException(message="Invalid category_id", status_code=status.HTTP_400_BAD_REQUEST)
+    return success_response(
+        data=ProductResponse.from_orm(product), message="Product created successfully")
 
-    product = Product(**product_data.dict())
-    db.add(product)
-    db.commit()
-    db.refresh(product)
-    return product
-
+   
 def get_product(db: Session, product_id: int):
-    return db.query(Product).filter(Product.id == product_id).first()
+    product = product_helper.get_product(db, product_id)
+    if not product:
+        raise CustomException(message="Product not found", status_code=status.HTTP_404_NOT_FOUND)
+    return success_response(
+        data=ProductResponse.from_orm(product),
+        message="Product fetched successfully"
+    )
 
-def get_all_products(db: Session):
-    return db.query(Product).all()
+def get_all_products(db: Session, skip: int = 0, limit: int = 100):
+    products = product_helper.get_all_products(db, skip, limit)
+    return success_response(
+    data=[ProductResponse.from_orm(p) for p in products],
+    message="Products fetched successfully"
+)
+
 
 def update_product(db: Session, product_id: int, updates: ProductUpdate):
-    product = get_product(db, product_id)
+    product = product_helper.get_product(db, product_id)
     if not product:
-        return None
+        raise CustomException(message="Product not found", status_code=status.HTTP_404_NOT_FOUND)
     
-    if updates.category_id is not None:
-        category = db.query(Category).filter(Category.id == updates.category_id).first()
-        if not category:
-            raise HTTPException(status_code=400, detail=f"Category with id={updates.category_id} does not exist")
- 
-    for key, value in updates.dict(exclude_unset=True).items():
-        setattr(product, key, value)
-    db.commit()
-    db.refresh(product)
-    return product
+    updated = product_helper.update_product(db, product, updates)
+    if not updated:
+            raise CustomException(message="Invalid category_id", status_code=status.HTTP_400_BAD_REQUEST)
+    return success_response(data=ProductResponse.from_orm(updated), message="Product updated successfully")
+
 
 def delete_product(db: Session, product_id: int):
-    product = get_product(db, product_id)
+    product = product_helper.get_product(db, product_id)
     if not product:
-        return None
-    db.delete(product)
-    db.commit()
-    return product
+        raise CustomException(message="Product not found", status_code=status.HTTP_404_NOT_FOUND)
+    deleted = product_helper.delete_product(db, product)
+    return success_response(data=ProductResponse.from_orm(deleted), message="Product deleted successfully")
+    
+
