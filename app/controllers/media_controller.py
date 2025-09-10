8d0225f86720236d7219fe1577d22a56620ec30e
Alok-Yadav45 <alokyadav@tothebyte.com>
2025-09-10 18:37:09 +0530

    completed upload table with all the validations and limit.

--- app/controllers/media_controller.py
+++ app/controllers/media_controller.py
@@ -3,21 +3,40 @@ from sqlalchemy.orm import Session
 from app.configs.database import get_db
 from app.services import media_service
 from app.schemas.media_schema import MediaOut, APIResponse
+from typing import List
 
 router = APIRouter()
 
-@router.post("/upload", response_model=APIResponse[MediaOut])
+@router.post("/upload", response_model=APIResponse[List[MediaOut]])
 async def upload_file(
     entity_type: str = Form(...),  
     entity_id: int = Form(...),
     file_type: str = Form(...),
-    file: UploadFile = File(...),
+    file: List[UploadFile] = File(...),
     db: Session = Depends(get_db)
 ):
    
    
 
-    return media_service.upload_media(db, file, entity_type, entity_id, file_type)
+    return media_service.upload_media_files(db, file, entity_type, entity_id, file_type)
+
+
+
+@router.put("/media/{media_id}", response_model=APIResponse[MediaOut])
+async def update_media(
+    media_id: int,
+    file_url: str = Form(None),
+    file_type: str = Form(None),
+    filename: str = Form(None),
+    new_file: UploadFile = File(None),
+    db: Session = Depends(get_db)
+):
+    update_data = {
+        "file_url": file_url,
+        "file_type": file_type,
+        "filename": filename
+    }
+    return media_service.update_media(db, media_id, update_data, new_file)
 
 
 @router.delete("/media/{media_id}", response_model=APIResponse)
